f2349f80e6c7ac076799b887198039696ca883c0
AlexisMartin <alexis.martin.44@gmail.com>
2023-01-04 10:24:05 +0100

    Back-end setup done

---
+++ back/data/index.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userIds = [
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+];
+
+export const users = [
+  {
+    _id: userIds[0],
+    firstName: "test",
+    lastName: "me",
+    email: "aaaaaaa@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p11.jpeg",
+    __v: 0,
+  },
+  {
+    _id: userIds[1],
+    firstName: "Steve",
+    lastName: "Ralph",
+    email: "thataaa@gmail.com",
+    password: "$!FEAS@!O)_IDJda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p3.jpeg",
+    __v: 0,
+  },
+  {
+    _id: userIds[2],
+    firstName: "Some",
+    lastName: "Guy",
+    email: "someguy@gmail.com",
+    password: "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+    picturePath: "p4.jpeg",
+    __v: 0,
+  },
+];
--- back/routes/users.js
+++ back/routes/users.js
@@ -1,8 +1,6 @@
 import express from "express";
 import {
   getUser,
-  getUserFriends,
-  addRemoveFriend,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
@@ -10,9 +8,5 @@ const router = express.Router();
 
 /* READ */
 router.get("/:id", verifyToken, getUser);
-router.get("/:id/friends", verifyToken, getUserFriends);
-
-/* UPDATE */
-router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
